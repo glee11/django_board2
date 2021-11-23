75daca83f324fd29ee398435e8312ec81d388995
glee11 <ehdvkflehd@gmail.com>
2021-11-23 15:38:24 +0900

    url 수정

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from . import settings
 
 urlpatterns = [
+    path('', include('acc.urls')),
     path('admin/', admin.site.urls),
     path('acc/', include('acc.urls')),
     path('board/', include('board.urls')),
